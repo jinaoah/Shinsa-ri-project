fc97acad3a47974636bb78d46561ae34089bc53b
hajin <w2128@e-mirim.hs.kr>
2022-09-17 18:45:26 +0900

    menu card start

---
+++ js/toggleMenu.js
@@ -0,0 +1,5 @@
+const toggleMenu = (toggleId, navListId) => {
+    const toggle = document.getElementById(toggleId);
+    const navList = document.getElementById(navListId);
+    const toggleIcon 
+}
